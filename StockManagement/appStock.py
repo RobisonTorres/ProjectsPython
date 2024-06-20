aaab208f664341a481a7bbb9a43291fa800d268e
RobisonTorres <robison.torres21@gmail.com>
2024-06-20 16:21:54 -0300

    updated code.

--- StockManagement/appStock.py
+++ StockManagement/appStock.py
@@ -5,16 +5,17 @@ def checkInput(function, product):
 
     # This function checks if the user's input is valid.
     try:
-        newProductPrice = float(input(f"Enter the {product} price: ").replace(' ', ''))
-        newProductQuantity = int(input(f"Enter the {product} quantity in stock: ").replace(' ', ''))
+        newProductPrice = float(input(f"Enter the {product}'s price: ").replace(' ', ''))
+        newProductQuantity = int(input(f"Enter the {product}'s quantity in stock: ").replace(' ', ''))
     
         if newProductPrice > 0 and newProductQuantity >= 0: 
             file[product.capitalize()] = [round(newProductPrice, 2), newProductQuantity]
             openSave.save_file(file)
-            print(f'The stock is updated - {product}.')
+            print(f'The stock has been updated - {product}.')
         else:
             print(f'Please, enter a valid price and quantity for {product}.')
             function(product)
+
     except ValueError:
         print(f'Please, enter a valid price and quantity for {product}.')
         function(product)
@@ -42,7 +43,7 @@ def update(nameProduct=False):
 
     # This function updates the products present in the stock.
     if nameProduct == False:
-        nameProduct = input(f'Enter the one you would like to update: ').capitalize()
+        nameProduct = input(f'Enter the product you would like to update: ').capitalize()
 
     if nameProduct not in list(file.keys()):
         print(f'{nameProduct} is not present in the stock.')
@@ -54,9 +55,26 @@ def update(nameProduct=False):
         
     checkInput(update, nameProduct)
 
+def delete():
+
+    # This function deletes product present in the stock.
+    if len(file) == 0:
+        print("Stock empty! There\'s nothing to delete.\n")
+        return read()
+    
+    deleteProduct = input('Enter the product you would like to delete: ').capitalize()
+
+    if deleteProduct not in list(file.keys()):
+        print(f'{deleteProduct} is not present in the stock.')
+        delete()
+    else:
+        del file[deleteProduct]
+        openSave.save_file(file)
+        print(f'{deleteProduct} has been deleted from the stock.')
+
 def read():
 
     # This function reads all the products present in the stock.
-    print(f"{'Name' : <15}|{'Price' : ^15}|{'Quantity' : >15}") 
+    print(f"{'Name' : <18}|{'Price' : ^18}|{'Quantity' : >18}") 
     for key, value in file.items():
-        print(f'{key: <15}|{value[0]: ^15}|{value[1]: >15}')
+        print(f'{key: <18}|{value[0]: ^18}|{value[1]: >18}')
--- StockManagement/main.py
+++ StockManagement/main.py
@@ -8,16 +8,16 @@ def main():
             '1. Add new product',
             '2. Update a product',
             '3. Show all products',
-            '4. Exit the program'
-        ]
-        lineBase = 47 * '*'
+            '4. Delete a product',
+            '5. Exit the program']
+        lineBase = 56 * '*'
 
         print(lineBase)
-        print('Menu - Stock'.center(47))
+        print('Menu - Stock'.center(56))
         print(lineBase)
         for item in menuOptions:
             space = len(lineBase) - len(item)
-            print(f'{"*" : <15}{item: ^5}{"*" : >{space - 15}} ')
+            print(f'{"*" : <18}{item: ^8}{"*" : >{space - 18}} ')
         print(lineBase)
         option = input('Your option: ')
         
@@ -30,6 +30,8 @@ def main():
             appStock.read()
             input('\nPress Enter to continue...')
         elif option == '4':
+            appStock.delete()
+        elif option == '5':
             print('Exit\n')
             break
         else:
