483ca506845f243c604efe7fc1400854f30a0857
RobisonTorres <robison.torres21@gmail.com>
2024-06-27 14:36:30 -0300

    updated code.

--- StockManagementSql/Static/script.js
+++ StockManagementSql/Static/script.js
@@ -1,10 +1,16 @@
+function goodString(name, price, quantity){
+
+    // This function matches any empty string.
+    return /^\s+$|^$/gi.test(name) || /^\s+$|^$/gi.test(price) || /^\s+$|^$/gi.test(quantity);
+}
+
 function checkInputAdd() {
 
     // This function validates user's input. 
     let input1 = document.getElementById("name_add").value;
     let input2 = document.getElementById("price_add").value;
     let input3 = document.getElementById("quantity_add").value;
-    let check = input1 == "" || input2 == "" || input3 == "";
+    let check = goodString(input1, input2, input3);
     if (check == true ) {
         alert("Please, fill in all the fields to add a new product.")
         return false;
@@ -19,7 +25,7 @@ function checkInputUpdate() {
     let input1 = document.getElementById("name_update").value;
     let input2 = document.getElementById("price_update").value;
     let input3 = document.getElementById("quantity_update").value;
-    let check = input1 == "" || input2 == "" || input3 == "";
+    let check = goodString(input1, input2, input3);
     if (check == true ) {
         alert("Please, fill in all the fields to update a product.")
         return false;
@@ -32,7 +38,7 @@ function checkInputDelete() {
 
     // This function validates user's input.
     let input1 = document.getElementById("product_delete").value;
-    let check = input1 == "";
+    let check = /^\s+$|^$/gi.test(input1);
     if (check == true ) {
         alert("Please, fill in the field to delete a product.")
         return false;
